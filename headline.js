67a4ba730543a0b9a332eee13332425907bea509
Julian Hollmann <julian.hollmann@googlemail.com>
2013-01-16 22:40:05 +0100

    Init

---
+++ headline.js
@@ -0,0 +1,40 @@
+(function($) {
+    $.headline = function(element, options) {
+        var defaults = {
+            inputMarkup: '<input type="text" />',
+            lineClass: 'line',
+            inputContainer: '#inputContainer'
+        };
+
+        var plugin = this;
+
+        plugin.settings = {};
+
+        var $element = $(element);
+        var lines, inputContainer;
+
+        plugin.init = function() {
+            plugin.settings = $.extend({}, defaults, options);
+
+            lines = $element.children('.' + plugin.settings.lineClass);
+            inputContainer = $(plugin.settings.inputContainer);
+
+            for(var i = 0; i < lines.length; ++i) {
+                var $input = $(plugin.settings.inputMarkup);
+                    $input.addClass('headlineInput_' + i);
+
+                inputContainer.append($input);
+            }
+
+        };
+
+        plugin.init();
+
+    }
+
+})(jQuery);
+
+$().ready(function() {
+    var $headline = $('.myHeadline');
+    $.headline($headline, {});
+});
