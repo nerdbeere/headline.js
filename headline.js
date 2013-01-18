3bf60e94bd7b90ce698ca84f10b28bc59303135d
Julian Hollmann <julian.hollmann@googlemail.com>
2013-01-18 15:47:00 +0100

    Static headline and some bugfixes

--- headline.js
+++ headline.js
@@ -6,9 +6,22 @@
             inputMarkup: '<input type="text" />',
             inputWrapperMarkup: '<p />',
             lineClass: 'line',
-            inputContainer: '#inputContainer',
+			lineMarkup: '<h2 />',
+            inputContainer: 'inputContainer',
             inputClass: 'headlineInput',
-            draggable: true
+			headlineContainerClass: 'headlineContainer',
+            draggable: true,
+			editable: false,
+			lines: [
+				{
+					text: '',
+					offset: 0
+				},
+				{
+					text: '',
+					offset: 0
+				}
+			]
         };
 
         var plugin = this;
@@ -16,7 +29,7 @@
         plugin.settings = {};
 
         var $element = $(element);
-        var lines, inputContainer;
+        var inputContainer;
 
         // initital 
         plugin.init = function() {
@@ -24,50 +37,82 @@
             plugin.settings = $.extend({}, defaults, options);
 
             // get all lines
-            lines = $('.' + plugin.settings.lineClass, $element);
+			var lines = plugin.settings.lines;
+			for(var i = 0; i < lines.length; i++){
+				var $line = $(plugin.settings.lineMarkup);
+				$line.addClass(plugin.settings.lineClass);
 
-            inputContainer = $(plugin.settings.inputContainer);
+				$span = $('<span />');
+				$span.text(lines[i].text);
+				$span.css('left', lines[i].offset);
+				$span.css('top', '0px !important');
+				$span.css('position', 'relative');
 
-            // loop through each line and build the necessary DOM
-            for(var i = 0; i < lines.length; i++) {
+				$line.append($span);
 
-                // get some DOM elements
-                var $input = $(plugin.settings.inputMarkup);
-                var $line = $(lines[i]);
-                var $inputWrapper = $(plugin.settings.inputWrapperMarkup);
+				$('.' + plugin.settings.headlineContainerClass, $element).append($line);
+			}
 
-                $input.data('key', i);
-                $input.addClass(plugin.settings.inputClass);
-
-                $inputWrapper.append($input);
-
-                $line.attr('id', plugin.settings.lineClass + '_' + i);
-                $line.append($('<span />'));
-                inputContainer.append($inputWrapper);
-            }
-
-            // init dragging
-            if(plugin.settings.draggable) {
-                $('span', '.line').draggable({
-                    containment: 'parent'
-                });
-            }
+			if(plugin.settings.editable) {
+				initEditMode();
+			}
 
             bindEvents();
         };
 
+		function initEditMode() {
+			$element.addClass('editable');
+			inputContainer = $('.' + plugin.settings.inputContainer, $element);
+			var lines = $('.' + plugin.settings.lineClass, $element);
+
+			// loop through each line and build the necessary DOM
+			for(var i = 0; i < lines.length; i++) {
+
+				// get some DOM elements
+				var $input = $(plugin.settings.inputMarkup);
+				var $line = $(lines[i]);
+				var $inputWrapper = $(plugin.settings.inputWrapperMarkup);
+				var lineText = $('span', $line).text();
+
+				$input.data('key', i);
+				$input.addClass(plugin.settings.inputClass);
+				$input.attr('placeholder', 'Line ' + Number(i + 1))
+
+				$inputWrapper.append($input);
+
+				$input.val(lineText);
+
+				$line.addClass(plugin.settings.lineClass + '_' + i);
+				inputContainer.append($inputWrapper);
+			}
+
+			// init dragging
+			if(plugin.settings.draggable) {
+				$('span', '.line').draggable({
+					containment: 'parent'
+				});
+			}
+		}
+
         function bindEvents() {
+
             $('.' + plugin.settings.inputClass).on('keydown', function(e) {
                 var $elem = $(this);
-                window.setTimeout(function() {
-                    addText($elem);
-                }, 0);
-                return true;
+
+				/**
+				 * This is a hack to make sure we already
+				 * have the content in the input element.
+				 */
+				window.setTimeout(function() {
+					addText($elem);
+				}, 0);
+
+				return true;
             });
         }
 
         function addText($elem) {
-            var $headlineElem = $('span', '#' + plugin.settings.lineClass + '_' + $elem.data('key'));
+            var $headlineElem = $('.' + plugin.settings.lineClass + '_' + $elem.data('key') + ' span', $element);
             var $tmpHeadline = $headlineElem.clone();
 
             $tmpHeadline.text($elem.val());
@@ -76,10 +121,22 @@
             var lineWidth = $headlineElem.parent().width();
             $tmpHeadline.remove();
 
-            if(textWidth < lineWidth) {
+            if(textWidth + getOffset($headlineElem) < lineWidth) {
                 $headlineElem.text($elem.val());
+				setNewValue($elem, $headlineElem, $headlineElem.text(), textWidth, lineWidth);
+				return true;
             }
+			setNewValue($elem, $headlineElem, $headlineElem.text(), textWidth, lineWidth);
+			return false;
         }
+
+		var prevVal = '';
+		function setNewValue($elem, $headlineElem, val, textWidth, lineWidth) {
+			if(val !== prevVal || textWidth + getOffset($headlineElem) > lineWidth) {
+				prevVal = val;
+				$elem.val(val);
+			}
+		}
 
         function getOffset($line) {
             return Number($line.css('left').split('px')[0]) || 0;
@@ -91,7 +148,7 @@
         return {
             getData: function() {
                 var linesArr = [];
-
+				var lines = $('.' + plugin.settings.lineClass, $element);
                 for(var i = 0; i < lines.length; i++) {
                     var $line = $('span', $(lines[i]));
                     var offset = getOffset($line);
@@ -115,7 +172,18 @@
 var myHeadline;
 $().ready(function() {
     var $headline = $('.myHeadline');
-    myHeadline = $.headline($headline, {});
+    var $headline2 = $('.myHeadline2');
 
-    console.log(myHeadline.getData())
+    myHeadline = $.headline($headline, {
+		editable: true,
+		lines: [
+			{ text: 'Welcome!', offset: 0 },
+			{ text: 'This is headline.js', offset: 30 }
+		]
+	});
+
+	myHeadline2 = $.headline($headline2, {
+		lines: myHeadline.getData()
+	});
+	prettyPrint();
 });
